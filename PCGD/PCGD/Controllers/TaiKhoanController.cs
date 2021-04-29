2f4be1e05296b6199a6d20da3ee35f1c4f9098fd
Sĩ Ben <dangtiensi.vn@gmail.com>
2021-04-30 00:04:45 +0700

    Summary

---
+++ PCGD/PCGD/Controllers/TaiKhoanController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PCGD.Models;
+
+namespace PCGD.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        private PCGDEntities db = new PCGDEntities();
+
+        // GET: TaiKhoan
+        public ActionResult Index()
+        {
+            return View(db.TaiKhoan.ToList());
+        }
+
+        // GET: TaiKhoan/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        // GET: TaiKhoan/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TaiKhoan/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,TaiKhoan1,MatKhau,NgayTao")] TaiKhoan taiKhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TaiKhoan.Add(taiKhoan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taiKhoan);
+        }
+
+        // GET: TaiKhoan/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        // POST: TaiKhoan/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,TaiKhoan1,MatKhau,NgayTao")] TaiKhoan taiKhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taiKhoan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taiKhoan);
+        }
+
+        // GET: TaiKhoan/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        // POST: TaiKhoan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            db.TaiKhoan.Remove(taiKhoan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PCGD/PCGD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PCGD.MvcApplication" Language="C#" %>
